3fd9b21b4608a4cd9288bfd663d5374b8f3b7c89
beatriz <designerbrm@gmail.com>
2025-06-04 07:31:10 +0200

    Nuevos cambios

--- scriptPP.js
+++ scriptPP.js
@@ -1,30 +1,30 @@
 const acceptBtn = document.getElementById('acceptBtn');
-    const rejectBtn = document.getElementById('rejectBtn');
-    const consentBox = document.getElementById('consentBox');
-    const mainContent = document.getElementById('mainContent');
+const rejectBtn = document.getElementById('rejectBtn');
+const consentBox = document.getElementById('consentBox');
 
-  
-    acceptBtn.addEventListener('click', () => {
-      localStorage.setItem('consentGiven', 'true');
-      consentBox.style.display = 'none';
-      mainContent.style.display = 'block';
-    });
 
-  
-    rejectBtn.addEventListener('click', () => {
-      localStorage.setItem('consentGiven', 'false');
-      window.location.href = 'https://www.google.com'; 
-    });
+acceptBtn.addEventListener('click', () => {
+    localStorage.setItem('consentGiven', 'true');
+    consentBox.style.display = 'none';
 
-    
-    window.onload = () => {
-      const consent = localStorage.getItem('consentGiven');
-      if (consent === 'true') {
+});
+
+
+rejectBtn.addEventListener('click', () => {
+    localStorage.setItem('consentGiven', 'false');
+    window.location.href = 'https://www.google.com'; 
+});
+
+
+window.onload = () => {
+    const consent = localStorage.getItem('consentGiven');
+    if (consent === 'true') {
         consentBox.style.display = 'none';
-        mainContent.style.display = 'block';
-      } else if (consent === 'false') {
+
+    } else if (consent === 'false') {
         window.location.href = 'https://www.google.com';
-      } else {
+    } else {
         consentBox.style.display = 'block';
-      }
-    };
+    }
+};
+
